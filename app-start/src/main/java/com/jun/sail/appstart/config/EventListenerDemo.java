567bbeb36b3fe7c92a6b47636c640d32a752e97c
spiritn <40299015+spiritn@users.noreply.github.com>
2020-06-25 16:51:46 +0800

    feat: add PostConstruct for app-start

--- app-start/src/main/java/com/jun/sail/appstart/config/EventListenerDemo.java
+++ app-start/src/main/java/com/jun/sail/appstart/config/EventListenerDemo.java
@@ -1,10 +1,14 @@
 package com.jun.sail.appstart.config;
 
 import com.jun.sail.appstart.constants.AppStartConstant;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
 
 /**
  * @author Jun
@@ -14,10 +18,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class EventListenerDemo {
 
+    @Getter
+    private HashMap<String, String> typeMap;
+
     @EventListener(classes = ApplicationReadyEvent.class)
-    public void init(){
+    public void processInit(){
 
         log.info(AppStartConstant.LOG_SPARATOR + "[ listener ApplicationReadyEvent ]");
+    }
+
+
+    /**
+     * 用来在属性注入后执行一些初始化方法，没有返回值和参数
+     *
+     * 可以用来初始化一些OSSClient连接对象，或者初始化常量map等
+     */
+    @PostConstruct
+    public void init(){
+        log.info(AppStartConstant.LOG_SPARATOR + "[ PostConstruct ]");
+
+        typeMap = new HashMap<>();
+        typeMap.put("1", "IOS");
+        typeMap.put("2", "ANDROID");
+        typeMap.put("3", "ANDROID-GOOGLEPLAY");
     }
 
 
