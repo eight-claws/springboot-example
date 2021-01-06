ae1378592917f9fe723a96c83699026ccb8f9e14
wangjun <jun27.wang@zhangmen.com>
2021-01-06 10:50:23 +0800

    feat：fix resource

--- springboot-feature/src/main/java/com/jun/sail/feature/resources/ResourceHandler.java
+++ springboot-feature/src/main/java/com/jun/sail/feature/resources/ResourceHandler.java
@@ -1,6 +1,7 @@
 package com.jun.sail.feature.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -17,9 +18,18 @@ public class ResourceHandler {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Value("classpath:/application.properties")
+    private Resource diResource;
+
+    @Value("file:///some/resource/path/myTemplate.txt")
+    private Resource fileResource;
+
     @PostConstruct
     public void testGetResource(){
         // ApplicationContext实现了ResourceLoader接口,所以下面可以getResource()
+
+        // 可以直接注入Resource
+        System.out.println(diResource.getDescription());
 
         // 从本地File System获取
         Resource resource = applicationContext.getResource("file:///E:/code/github/doc-for-spring/pages/spring/Overview.md");
